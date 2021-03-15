b47945fdeeb5d39f0e78745b44c14e361414ec35
Mike Bromberek <mikebromberek@gmail.com>
2021-03-14 19:25:12 -0500

    Add logging

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ All rights reserved.
 
 # First party classes
 import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 
 # Third party imports
@@ -25,6 +26,24 @@ def create_app(config_class=Config):
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/personal_site.log',
+                                           maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('personal_site startup')
 
     return app
 
