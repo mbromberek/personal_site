f16cd4cdef985b142177b8a9002a0ae403d6a878
Mike Bromberek <mikebromberek@gmail.com>
2021-03-21 09:17:30 -0500

    Added message after login

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -19,24 +19,26 @@ from app.auth.forms import LoginForm
 @bp.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
-    return render_template('auth/login.html', title='Sign In', form=form)
+    # return render_template('auth/login.html', title='Sign In', form=form)
     # if current_user.is_authenticated:
     #     return flask.redirect(flask.url_for('main.index'))
     # form = LoginForm()
-    # if form.validate_on_submit():
-    #     user = User.query.filter_by(username=form.username.data).first()
-    #     if user is None or not user.check_password(form.password.data):
-    #         flask.flash(_('Invalid username or password'))
-    #         return flask.redirect(flask.url_for('auth.login'))
-    #     login_user(user, remember=form.remember_me.data)
-    #     next_page = request.args.get('next')
-    #     if not next_page or url_parse(next_page).netloc != '':
-    #         next_page = flask.url_for('main.index')
-    #     return flask.redirect(next_page)
-    # return flask.render_template('auth/login.html', title='Sign In', form=form)
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('main.index'))
+        # user = User.query.filter_by(username=form.username.data).first()
+        # if user is None or not user.check_password(form.password.data):
+        #     flask.flash(_('Invalid username or password'))
+        #     return flask.redirect(flask.url_for('auth.login'))
+        # login_user(user, remember=form.remember_me.data)
+        # next_page = request.args.get('next')
+        # if not next_page or url_parse(next_page).netloc != '':
+        #     next_page = flask.url_for('main.index')
+        # return flask.redirect(next_page)
+    return render_template('auth/login.html', title='Sign In', form=form)
 
 
 @bp.route('/logout')
 def logout():
     logout_user()
-    return flask.redirect(flask.url_for('main.index'))
+    return redirect(flask.url_for('main.index'))
