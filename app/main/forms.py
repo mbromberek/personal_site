3d865c099362433be11c619e4a129a2a1dea0e40
Mike Bromberek <mikebromberek@gmail.com>
2021-03-28 10:11:05 -0500

    Load Edit Workout page, currently does not save the workout

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+
+'''
+BSD 3-Clause License
+Copyright (c) 2021, Mike Bromberek
+All rights reserved.
+'''
+
+# Third party classes
 from flask import request
-from wtforms import StringFiel, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
+
+
+class WorkoutForm(FlaskForm):
+    type = StringField('Type', validators=[DataRequired()])
+    dttm = StringField('Date Time', validators=[DataRequired()])
+    duration = StringField('Duration', validators=[DataRequired()])
+    distance = StringField('Distance', validators=[DataRequired()])
+    notes = TextAreaField('Say something', validators=[DataRequired(), Length(min=0, max=500)])
+    submit = SubmitField('Submit')
--- app/main/routes.py
+++ app/main/routes.py
@@ -6,13 +6,14 @@ Copyright (c) 2021, Mike Bromberek
 All rights reserved.
 '''
 
-# Third party imports
+# Third party classes
 from flask import render_template, flash, redirect, url_for, request, g, \
     jsonify, current_app
 from flask_login import current_user, login_required
 
-# Custom imports
+# Custom classes
 from app.main import bp
+from app.main.forms import EmptyForm, WorkoutForm
 
 @bp.route('/')
 @bp.route('/index')
@@ -23,10 +24,30 @@ def index():
 
     return render_template('index.html', title='Home Page', workouts=workouts)
 
-@bp.route('/workouts')
+@bp.route('/workouts', methods=['GET','POST'])
 @login_required
 def workouts():
-    user = {'displayname': 'Mike'}
+    form = EmptyForm()
+    if form.validate_on_submit():
+        print("redirect(url_for('main.edit_workout'))")
+        return redirect(url_for('main.edit_workout'))
+
     workouts = [{'type':'Running', 'duration':'20m 56s', 'distance': '3.11', 'pace': '6m 44s'}, {'type':'Running', 'duration':'3h 35m 53s', 'distance': '26.2', 'pace': '8m 13s'}]
 
-    return render_template('workouts.html', title='Workouts', user=user, workouts=workouts)
+    return render_template('workouts.html', title='Workouts', workouts=workouts, form=form)
+
+@bp.route('/edit_workout', methods=['GET','POST'])
+@login_required
+def edit_workout():
+    print("edit_workout")
+    form = WorkoutForm()
+    # if form.validate_on_submit():
+    #     current_user.username = form.username.data
+    #     current_user.about_me = form.about_me.data
+    #     db.session.commit()
+    #     flash('Your changes have been saved.')
+    #     return redirect(url_for('main.edit_workout'))
+    # elif request.method == 'GET':
+    #     form.username.data = current_user.username
+    #     form.about_me.data = current_user.about_me
+    return render_template('edit_workout.html', title='Edit Workout', form=form)
