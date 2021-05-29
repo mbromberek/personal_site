1e635f6f7bffdbf4d46983855acc61e0a410795e
Mike Bromberek <mikebromberek@gmail.com>
2021-05-29 11:24:01 -0500

    Set login email field to be validated it is in email format

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -17,7 +17,7 @@ from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Le
 from app.models import User
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
