23b305fc6aac92951291b5d64ee5da10be0f86d1
Mike Bromberek <mikebromberek@gmail.com>
2021-12-24 14:18:54 -0600

    Added duration in seconds to be exported

--- app/utils/const.py
+++ app/utils/const.py
@@ -13,11 +13,12 @@ NBR_WK_COMP = 10
 SHOE_MILE_AGE_WARNING = 300
 SHOE_MILE_AGE_SHOULD_RETIRE = 450
 
-EXPORT_FIELDS = ['Type','Date','Duration h:m:s','Distance','Pace', 'Gear','HR','Category','Calories', 'Notes','Notes+','Elevation', 'Elevation Up', 'Elevation Down', 'Location', 'Training Type', 'Weather','Clothes']
+EXPORT_FIELDS = ['Type','Date','Duration h:m:s','Duration Seconds','Distance','Pace', 'Gear','HR','Category','Calories', 'Notes','Notes+','Elevation', 'Elevation Up', 'Elevation Down', 'Location', 'Training Type', 'Weather','Clothes']
 EXPORT_FIELD_MAPPING = {
     'Type':'type',
     'Date':'wrkt_dttm',
     'Duration h:m:s':'duration',
+    'Duration Seconds':'dur_sec',
     'Distance':'dist_mi',
     'Pace':'pace',
     'Gear':'gear',
