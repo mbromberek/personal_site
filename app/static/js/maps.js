f732dcddfb34772f99bde7a794c47f05af4138df
Mike Bromberek <mikebromberek@gmail.com>
2022-03-13 11:40:19 -0500

    moved creation of marker to a separate function

--- app/static/js/maps.js
+++ app/static/js/maps.js
@@ -1,14 +1,52 @@
+var endCircle = {
+    radius: 6,
+    fillColor: 'red',
+    color: '#000',
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.7
+}
+var startCircle = {
+    radius: 6,
+    fillColor: 'green',
+    color: '#000',
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.7
+}
+var blueCircle = {
+    radius: 6,
+    fillColor: 'blue',
+    color: '#000',
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+}
+var whiteCircle = {
+    radius: 6,
+    fillColor: 'white',
+    color: '#000',
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+}
+
+var wrktLine = {
+    color: 'blue',
+    weight: 3,
+    opacity: 0.6
+}
+
 function initMap(map_json) {
     console.log('maps.js initMap');
-    // map_det = JSON.parse(map_json);
-    console.log(map_json);
+    // console.log(map_json);
     apiKey = map_json.key;
     zoom = map_json.zoom;
     center_lon = map_json.center.lon;
     center_lat = map_json.center.lat;
     lat_lon = map_json.lat_lon;
 
-
+    mile_marker_color = 'white'
     var run_map_center = { pos:[center_lat, center_lon], zoom:zoom };
     var greenIcon = new L.Icon({
         iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
@@ -20,72 +58,18 @@ function initMap(map_json) {
         shadowSize: [20, 20]
     });
 
-    var redCircle = {
-        radius: 6,
-        fillColor: 'red',
-        color: '#000',
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
-    }
-    var greenCircle = {
-        radius: 6,
-        fillColor: 'green',
-        color: '#000',
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
-    }
-    var blueCircle = {
-        radius: 6,
-        fillColor: 'blue',
-        color: '#000',
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
-    }
-    var whiteCircle = {
-        radius: 6,
-        fillColor: 'white',
-        color: '#000',
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
-    }
-
-    var blueLine = {
-        color: 'blue',
-        weight: 3,
-        opacity: 0.6
-    }
 
     var polylinePoints = lat_lon;
-    var start_mark = {position:lat_lon[0], icon:greenCircle, popup: 'Run Start'}
-    var end_mark = {position:lat_lon[lat_lon.length -1], icon:redCircle, popup: 'Run End'};
+    var start_mark = {position:lat_lon[0], icon:startCircle, popup: 'Run Start'}
+    var end_mark = {position:lat_lon[lat_lon.length -1], icon:endCircle, popup: 'Run End'};
     // var mile_one_mark = {position:[40.732828164473200, -89.57437014207240], icon:whiteCircle, popup: 'Mile 1'};
 
     var milePoints = [];
     // console.log(map_json.mile_markers);
 
     map_json.mile_markers.forEach(function(marker, index){
-        console.log(marker);
-        var mile_marker_json = {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [marker.lat, marker.lon]
-            },
-            "properties": {
-                "text": marker.mile_nbr.toString(),
-                "radius": 7,
-                "fillColor": 'white',
-                "color": '#000',
-                "weight": 1,
-                "opacity": 1,
-                "fillOpacity": "0.8"
-            }
-        };
-        milePoints.push(mile_marker_json);
+        // console.log(marker);
+        milePoints.push(create_marker(marker, mile_marker_color));
     });
     // console.log(milePoints);
 
@@ -100,7 +84,7 @@ function initMap(map_json) {
       accessToken: apiKey
     }).addTo(map);
 
-    var polyline = L.polyline(polylinePoints, blueLine).addTo(map);
+    var polyline = L.polyline(polylinePoints, wrktLine).addTo(map);
 
     // L.marker(start_mark['position'], {icon: start_mark['icon']}).addTo(map).bindPopup(start_mark['popup']);
     L.circleMarker(start_mark['position'], start_mark['icon']) .addTo(map).bindPopup(start_mark['popup']);
@@ -108,8 +92,6 @@ function initMap(map_json) {
 
     var mileMarkers = new L.geoJson(milePoints, {
         pointToLayer: function(feature, latlng) {
-            // console.log(latlng.lat + ' ' + latlng.lng);
-            // return new L.CircleMarker([latlng.lng, latlng.lat], {radius: feature.properties.radius});
             return new L.CircleMarker([latlng.lng, latlng.lat],  feature.properties);
         },
         onEachFeature: function(feature, layer) {
@@ -134,4 +116,24 @@ function initMap(map_json) {
     }).addTo(map);
 
     console.log('End: leaflet_maps initMap');
+}
+
+function create_marker(marker, marker_color){
+    var mile_marker_json = {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [marker.lat, marker.lon]
+        },
+        "properties": {
+            "text": marker.mile_nbr.toString(),
+            "radius": 7,
+            "fillColor": marker_color,
+            "color": '#000',
+            "weight": 1,
+            "opacity": 1,
+            "fillOpacity": "0.8"
+        }
+    };
+    return mile_marker_json
 }
