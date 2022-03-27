f79ed130ade631c604cd90c38fdb116223569d87
Mike Bromberek <mikebromberek@gmail.com>
2022-03-27 18:51:30 -0500

    Adjust size of map on workout page

--- app/main/routes.py
+++ app/main/routes.py
@@ -518,7 +518,7 @@ def workout():
             map_dict['key'] = current_app.config['MAPBOX_API_KEY']
 
             map_dict['center'] = genMap.calc_center(lats=[lat_max, lat_min], lons=[lon_max, lon_min])
-            map_dict['zoom'] = genMap.calc_zoom(lats=[lat_min, lat_max], lons=[lon_min, lon_max], img_dim={'height':1100, 'width':1100})
+            map_dict['zoom'] = genMap.calc_zoom(lats=[lat_min, lat_max], lons=[lon_min, lon_max], img_dim={'height':1600, 'width':1600})
 
             print('zoom: ' + str(map_dict['zoom']))
             print('center:' + str(map_dict['center']))
