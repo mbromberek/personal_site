19e1479a91157192aa8062fb54f34cbc70146569
Mike Bromberek <mikebromberek@gmail.com>
2022-05-08 05:46:03 -0500

    Default values of 0 for gear_usage page

--- db_objects/vw gear_usage.sql
+++ db_objects/vw gear_usage.sql
@@ -1,8 +1,8 @@
 CREATE or replace VIEW fitness.gear_usage AS
 SELECT gear.nm,
     count(wrkt.id) AS usage_count,
-    sum(wrkt.dist_mi) AS tot_dist,
-    sum(wrkt.dur_sec) AS tot_dur_sec,
+    coalesce(sum(wrkt.dist_mi),0) AS tot_dist,
+    coalesce(sum(wrkt.dur_sec),0) AS tot_dur_sec,
     max(wrkt.wrkt_dttm) AS latest_workout,
     min(wrkt.wrkt_dttm) AS first_workout,
     gear.prchse_dt,
@@ -13,10 +13,11 @@ SELECT gear.nm,
     gear.company,
     gear.id AS gear_id,
     gear.user_id
-   FROM fitness.gear gear
-     LEFT JOIN fitness.workout wrkt ON gear.id = wrkt.gear_id AND gear.user_id = wrkt.user_id
-  WHERE gear.type::text = ANY (ARRAY['Shoe'::character varying::text, 'Bike'::character varying::text])
-  GROUP BY wrkt.user_id, gear.nm, gear.id, gear.prchse_dt, gear.price, gear.retired, gear.confirmed, gear.type, gear.company
+FROM fitness.gear gear
+LEFT JOIN fitness.workout wrkt
+  ON gear.id = wrkt.gear_id AND gear.user_id = wrkt.user_id
+WHERE gear.type::text = ANY (ARRAY['Shoe'::character varying::text, 'Bike'::character varying::text])
+GROUP BY wrkt.user_id, gear.nm, gear.id, gear.prchse_dt, gear.price, gear.retired, gear.confirmed, gear.type, gear.company
 ;
 
 GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA fitness TO app_role;
