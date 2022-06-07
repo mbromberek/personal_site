d8bc6b5eb44950cff198089300376d001e7ed66c
Mike Bromberek <mikebromberek@gmail.com>
2022-06-06 19:30:50 -0500

    Comment out event_pages section

--- app/main/route/event_pages.py
+++ app/main/route/event_pages.py
@@ -27,24 +27,24 @@ from app import logger
 @login_required
 def event():
     logger.info('event')
-    usr_id = current_user.id
-    event_id = request.args.get('event')
-    logger.info('event: ' + str(event_id) + ' for user: ' + str(usr_id))
-
-    map_dict = {}
-    map_dict['key'] = current_app.config['MAPBOX_API_KEY']
-    map_dict['max_zoom'] = current_app.config['MAP_MAX_ZOOM']
-
-    map_dict['center'] = {'lat':'40.703545803451426', 'lon':'-74.0032196044922'}
-    map_dict['zoom'] = 11
-    map_dict['lat_lon'] = []
-    map_dict['mile_markers'] = []
-
-    loc_lst = []
-    hotel_det = {'name':'Holiday Inn', 'type':'hotel', 'lat':'40.756350', 'lon':'-73.993120'}
-    race_start = {'name':'New York Marathon Start', 'type':'race_start', 'lat':'40.603282429411436', 'lon':'-74.05486822128297'}
-    loc_lst = [hotel_det, race_start]
-
-    map_dict['loc_lst'] = loc_lst
-
-    return render_template('event.html', map_dict=map_dict, destPage='event')
+    # usr_id = current_user.id
+    # event_id = request.args.get('event')
+    # logger.info('event: ' + str(event_id) + ' for user: ' + str(usr_id))
+    #
+    # map_dict = {}
+    # map_dict['key'] = current_app.config['MAPBOX_API_KEY']
+    # map_dict['max_zoom'] = current_app.config['MAP_MAX_ZOOM']
+    #
+    # map_dict['center'] = {'lat':'40.703545803451426', 'lon':'-74.0032196044922'}
+    # map_dict['zoom'] = 11
+    # map_dict['lat_lon'] = []
+    # map_dict['mile_markers'] = []
+    #
+    # loc_lst = []
+    # hotel_det = {'name':'Holiday Inn', 'type':'hotel', 'lat':'40.756350', 'lon':'-73.993120'}
+    # race_start = {'name':'New York Marathon Start', 'type':'race_start', 'lat':'40.603282429411436', 'lon':'-74.05486822128297'}
+    # loc_lst = [hotel_det, race_start]
+    #
+    # map_dict['loc_lst'] = loc_lst
+    #
+    # return render_template('event.html', map_dict=map_dict, destPage='event')
