876c3ab06a61f9acd3888695d4aba1486425cb5f
Mike Bromberek <mikebromberek@gmail.com>
2022-06-17 19:18:05 -0500

    add comments

--- app/static/js/settings.js
+++ app/static/js/settings.js
@@ -1,3 +1,6 @@
+/*
+loads passed in token to clipboard, then makes message in passed element visible for 2 seconds. 
+*/
 async function copyToken(token_full, ele_id){
     navigator.clipboard.writeText(token_full);
     document.getElementById('token_copied').style.display='inline';
@@ -5,6 +8,9 @@ async function copyToken(token_full, ele_id){
     document.getElementById('token_copied').style.display='none';
 }
 
+/*
+Pause JavaScript that called this for passed in time in milliseconds
+*/
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
