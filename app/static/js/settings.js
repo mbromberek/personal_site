0d7a8d3dc27f3872bcf550925185d4ae78c22873
Mike Bromberek <mikebromberek@gmail.com>
2022-06-17 19:14:45 -0500

    Organize layout using CSS classes

--- app/static/js/settings.js
+++ app/static/js/settings.js
@@ -1,6 +1,7 @@
-async function copyToken(ele_id){
+async function copyToken(token_full, ele_id){
+    navigator.clipboard.writeText(token_full);
     document.getElementById('token_copied').style.display='inline';
-    await sleep(1000);
+    await sleep(2000);
     document.getElementById('token_copied').style.display='none';
 }
 
