54bb4e0cf428b84ff823fa7fcebfe7ac110a31b2
Mike Bromberek <mikebromberek@gmail.com>
2022-09-05 19:08:00 -0500

    Return error if cannot get updates from GR

--- app/api/books.py
+++ app/api/books.py
@@ -28,9 +28,9 @@ from app.model.book import Book
 def refresh_book_status():
     logger.info('refresh_book_status')
     usr_id = token_auth.current_user().id
-    books = refresh_books(usr_id)
+    books, status = refresh_books(usr_id)
     logger.debug(books)
-    return jsonify(books), 200
+    return jsonify(books), status
 
 def refresh_books(usr_id):
     # If not original user do not perform refresh
@@ -44,31 +44,41 @@ def refresh_books(usr_id):
 
     books = []
 
-    logger.debug('START: Get Current Reading Books')
-    # 1) Get Books Currently being read on GoodReads
-    Feed = feedparser.parse(current_reading)
-    # TODO If cannot connect what should happen?
-    gr_current_books = Feed.entries
-    logger.debug('END: Get Current Reading Books')
+    try:
+        logger.debug('START: Get Current Reading Books')
+        # Get Books Currently being read on GoodReads
+        Feed = feedparser.parse(current_reading)
+        if Feed.status != 200:
+            logger.error(Feed.status)
+            return [], Feed.status
+        gr_current_books = Feed.entries
+        logger.debug('END: Get Current Reading Books')
 
-    # 2) Convert from GR JSON feed format to Book Dictionary format
+        # Get 2 most recently read books
+        logger.debug('START: Get Read Books')
+        Feed = feedparser.parse(read_feed)
+        if Feed.status != 200:
+            logger.error(Feed.status)
+            return [], Feed.status
+        read_books = Feed.entries
+        logger.debug('END: Get Read Books')
+    except:
+        return [], 400
+
+
+    # Convert from GR JSON feed format to Book Dictionary format
     gr_book_lst = []
     for gr_book in gr_current_books:
         gr_book_lst.append(Book.GR_to_dict(gr_book, usr_id, curr_reading_val))
 
-    # 3) Get 2 most recently read books
-    logger.debug('START: Get Read Books')
-    Feed = feedparser.parse(read_feed)
-    read_books = Feed.entries
-    logger.debug('END: Get Read Books')
     gr_book_lst.append(Book.GR_to_dict(read_books[0], usr_id, read_val))
     gr_book_lst.append(Book.GR_to_dict(read_books[1], usr_id, read_val))
 
-    # 3) Get Books from DB
+    # Get Books from DB
     query = Book.query.filter_by(user_id=usr_id)
     db_books = query.all()
 
-    # 4) Loop through books in DB, and delete any not in gr_book_lst
+    # Loop through books in DB, and delete any not in gr_book_lst
     # Keep track of GR books that are not already in DB
     for db_book in db_books:
         match = False
@@ -84,7 +94,7 @@ def refresh_books(usr_id):
         else:
             books.append(db_book.to_dict())
 
-    # 5) Insert GR books that are not already in DB
+    # Insert GR books that are not already in DB
     for gr_book in gr_book_lst:
         if gr_book['already_exist'] == False:
             book = Book.from_dict(gr_book)
@@ -92,7 +102,7 @@ def refresh_books(usr_id):
             db.session.commit()
             books.append(book.to_dict())
 
-    return books
+    return books, 200
         
 
     # loop through list of GR current books and add any books not in remaining list of current books from database
