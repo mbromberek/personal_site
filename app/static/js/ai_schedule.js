c672dcbdbdfcd3cb4f63b5ffe91d4e0d316d0cfa
Mike Bromberek <mikebromberek@gmail.com>
2023-08-10 16:34:31 -0500

    Set panel_det from JSON with id
    Instead of copying data from selected schedule element to panel detail it uses the JSON data.
    Passes index instead of name of schedule element ID

--- app/static/js/ai_schedule.js
+++ app/static/js/ai_schedule.js
@@ -1,9 +1,10 @@
 var curr_sch_id = '';
+var prog_schedule = '';
 
 function loadSchedule(response){
   console.log(response);
   
-  let prog_schedule = response['prog_schedule'];
+  prog_schedule = response['prog_schedule'];
   let sch_lst_ele = document.getElementById('sch_lst');
   let j=0;
   //Get Current Day and Current Time to find first element that is close to current day/time to highlight
@@ -19,14 +20,14 @@ function loadSchedule(response){
     template_clone.querySelector("#day").innerHTML = panel['day'];
     template_clone.querySelector("#start_time").innerHTML = panel['start_time'];
     template_clone.querySelector("#end_time").innerHTML = panel['end_time'];
-    template_clone.querySelector("#panelists").innerHTML = panel['panelists'];
     
     template_clone.querySelector("#description").innerHTML = panel['description'];
-    template_clone.querySelector("#sch_card").setAttribute("onclick", "javascript: showDescription('card_"+j+"');");
+    // template_clone.querySelector("#sch_card").setAttribute("onclick", "javascript: showDescription('card_"+j+"');");
+    template_clone.querySelector("#sch_card").setAttribute("onclick", "javascript: showDescription('"+i+"');");
     template_clone.querySelector("#sch_card").classList.add('sch_card_color_'+j%2);
     template_clone.querySelector("#sch_card").classList.add('sch_card_panel_' + panel['panel_type']);
     // template_clone.querySelector("#sch_card").classList.add('sch_card_panel_guest');
-    template_clone.querySelector("#sch_card").setAttribute("id", 'card_'+j);
+    template_clone.querySelector("#sch_card").setAttribute("id", 'card_'+i);
     
     sch_lst_ele.appendChild(template_clone);
     j++;
@@ -34,7 +35,7 @@ function loadSchedule(response){
 
   //Default show panel description on page load for Desktop
   if (getWidth() >=600){
-    showDescription("card_0");
+    showDescription("0");
   }
 
 }
@@ -43,21 +44,22 @@ var showDescription = function(panel_id){
   console.log(panel_id);
   let panel_ele = document.getElementById('panel_det');
 
-  let sch_ele = document.getElementById(panel_id);
+  let sch_ele = document.getElementById('card_' + panel_id);
   sch_ele.classList.add("selected_panel");
   if (curr_sch_id != ''){
-    document.getElementById(curr_sch_id).classList.remove("selected_panel");
+    document.getElementById('card_'+curr_sch_id).classList.remove("selected_panel");
     // panel_ele.classList.remove("sch_card_panel_staff");
   }
   curr_sch_id = panel_id;
-  // console.log(sch_ele.querySelector('#description').innerHTML);
-  panel_ele.querySelector("#title").innerHTML = sch_ele.querySelector("#title").innerHTML;
-  panel_ele.querySelector("#room").innerHTML = sch_ele.querySelector("#room").innerHTML;
-  panel_ele.querySelector("#day").innerHTML = sch_ele.querySelector("#day").innerHTML;
-  panel_ele.querySelector("#start_time").innerHTML = sch_ele.querySelector("#start_time").innerHTML;
-  panel_ele.querySelector("#end_time").innerHTML = sch_ele.querySelector("#end_time").innerHTML;
-  panel_ele.querySelector("#panelists").innerHTML = sch_ele.querySelector("#panelists").innerHTML;
-  panel_ele.querySelector("#description").innerHTML = sch_ele.querySelector("#description").innerHTML;
+  let sel_sch_det = prog_schedule[panel_id];
+
+  panel_ele.querySelector("#title").innerHTML = sel_sch_det['title'];
+  panel_ele.querySelector("#room").innerHTML = sel_sch_det["room"];
+  panel_ele.querySelector("#day").innerHTML = sel_sch_det["day"];
+  panel_ele.querySelector("#start_time").innerHTML = sel_sch_det["start_time"];
+  panel_ele.querySelector("#end_time").innerHTML = sel_sch_det["end_time"];
+  panel_ele.querySelector("#panelists").innerHTML = sel_sch_det["panelists"];
+  panel_ele.querySelector("#description").innerHTML = sel_sch_det["description"];
   panel_ele.style.display = 'inline-block';
   
   
@@ -65,7 +67,7 @@ var showDescription = function(panel_id){
 
 function closeDescription(){
   console.log("closeDescription");
-  document.getElementById(curr_sch_id).classList.remove("selected_panel");
+  document.getElementById("card_"+curr_sch_id).classList.remove("selected_panel");
   document.getElementById('panel_det').style.display = 'none';
   curr_sch_id = '';
   
