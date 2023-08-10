ac3c2c8f27c48f583b79b8f51b26618ad13ab04e
Mike Bromberek <mikebromberek@gmail.com>
2023-08-10 15:57:52 -0500

    Show panel desc by default on desktop
    hide on mobile

--- app/static/js/ai_schedule.js
+++ app/static/js/ai_schedule.js
@@ -24,25 +24,33 @@ function loadSchedule(response){
     template_clone.querySelector("#description").innerHTML = panel['description'];
     template_clone.querySelector("#sch_card").setAttribute("onclick", "javascript: showDescription('card_"+j+"');");
     template_clone.querySelector("#sch_card").classList.add('sch_card_color_'+j%2);
+    template_clone.querySelector("#sch_card").classList.add('sch_card_panel_' + panel['panel_type']);
+    // template_clone.querySelector("#sch_card").classList.add('sch_card_panel_guest');
     template_clone.querySelector("#sch_card").setAttribute("id", 'card_'+j);
     
     sch_lst_ele.appendChild(template_clone);
     j++;
   }
-  // showDescription("card_0");
+
+  //Default show panel description on page load for Desktop
+  if (getWidth() >=600){
+    showDescription("card_0");
+  }
 
 }
 
 var showDescription = function(panel_id){
   console.log(panel_id);
+  let panel_ele = document.getElementById('panel_det');
+
   let sch_ele = document.getElementById(panel_id);
   sch_ele.classList.add("selected_panel");
   if (curr_sch_id != ''){
     document.getElementById(curr_sch_id).classList.remove("selected_panel");
+    // panel_ele.classList.remove("sch_card_panel_staff");
   }
   curr_sch_id = panel_id;
-  console.log(sch_ele.querySelector('#description').innerHTML);
-  let panel_ele = document.getElementById('panel_det');
+  // console.log(sch_ele.querySelector('#description').innerHTML);
   panel_ele.querySelector("#title").innerHTML = sch_ele.querySelector("#title").innerHTML;
   panel_ele.querySelector("#room").innerHTML = sch_ele.querySelector("#room").innerHTML;
   panel_ele.querySelector("#day").innerHTML = sch_ele.querySelector("#day").innerHTML;
@@ -53,4 +61,22 @@ var showDescription = function(panel_id){
   panel_ele.style.display = 'inline-block';
   
   
+}
+
+function closeDescription(){
+  console.log("closeDescription");
+  document.getElementById(curr_sch_id).classList.remove("selected_panel");
+  document.getElementById('panel_det').style.display = 'none';
+  curr_sch_id = '';
+  
+}
+
+function getWidth() {
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  );
 }
