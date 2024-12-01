15fd8cb70a6cc0bef3204369adc23f040f114797
Mike Bromberek <mikebromberek@gmail.com>
2024-12-01 17:58:30 -0600

    Clean up code for yearly mileage

--- app/static/js/dash_viz_yr_mileage.js
+++ app/static/js/dash_viz_yr_mileage.js
@@ -14,11 +14,7 @@ function initYrMileageChart(yr_lst, chart_name) {
     margin = 200,
     chart_width = yr_mileage_chart.attr("width") - margin,
     chart_height = yr_mileage_chart.attr("height") - margin;
-  // let chart = d3.select('#one_ring_run_chart'), 
-  //   margin = 200,
-  //   width = chart.attr("width") - margin,
-  //   height = chart.attr("height") - margin;
-  console.log('width: ' + chart_width + ' height: ' + chart_height);
+  // console.log('width: ' + chart_width + ' height: ' + chart_height);
   
   yr_mileage_chart.append("text")
     .attr("transform", "translate(100,0)")
@@ -33,96 +29,90 @@ function initYrMileageChart(yr_lst, chart_name) {
   chart_g = yr_mileage_chart.append("g")
           .attr("transform", "translate(" + 100 + "," + 100 + ")");
   
-  // d3.csv("xyz.csv", function(error, data) {
-  //     if (error) {
-  //         throw error;
-  //     }
-      let data = yr_lst
-  
-      chart_x.domain(data.map(function(d) { return d.year; }));
-      // chart_x.bandwidth(chart_x.bandwidth() + 25)
-      chart_y.domain([0, d3.max(data, function(d) { return d.tot_dist; })]);
-  
-      chart_g.append("g")
-        .attr("transform", "translate(0," + chart_height + ")")
-        .call(d3.axisBottom(chart_x))
-        .append("text")
-        .attr("y", chart_height - 260)
-        .attr("x", chart_width - 200)
-        .attr("text-anchor", "end")
-        .attr("fill", "black")
-        .attr("font-size", "20px")
-        .attr("font-family", "Saira")
-        .text("Year");
-  
-      chart_g.append("g")
-        .call(d3.axisLeft(chart_y).tickFormat(function(d){
-          return d;
-        }).ticks(10))
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        // .attr("y", 6)
-        .attr("dy", "-3.1em")
-        .attr("text-anchor", "end")
-        .attr("fill", "black")
-        .attr("font-size", "20px")
-        .text("Distance");
-  
-      chart_g.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .on("mouseover", onMouseOver) //Add listener for the mouseover event
-        .on("mouseout", onMouseOut)   //Add listener for the mouseout event
-        .attr("x", function(d) { return chart_x(d.year); })
-        .attr("y", function(d) { return chart_y(d.tot_dist); })
-        .attr("width", chart_x.bandwidth())
-        .transition()
-        .ease(d3.easeLinear)
-        .duration(400)
-        .delay(function (d, i) {
-          return i * 50;
-        })
-        .attr("height", function(d) { return chart_height - chart_y(d.tot_dist); });
-  // });
+  chart_x.domain(yr_lst.map(function(d) { return d.year; }));
+  chart_y.domain([0, d3.max(yr_lst, function(d) { return d.tot_dist; })]);
+
+  chart_g.append("g")
+    .attr("transform", "translate(0," + chart_height + ")")
+    .call(d3.axisBottom(chart_x))
+    .append("text")
+    .attr("y", chart_height - 260)
+    .attr("x", chart_width - 200)
+    .attr("text-anchor", "end")
+    .attr("fill", "black")
+    .attr("font-size", "20px")
+    .attr("font-family", "Saira")
+    .text("Year");
+
+  chart_g.append("g")
+    .call(d3.axisLeft(chart_y).tickFormat(function(d){
+      return d;
+    }).ticks(10))
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    // .attr("y", 6)
+    .attr("dy", "-3.1em")
+    .attr("text-anchor", "end")
+    .attr("fill", "black")
+    .attr("font-size", "20px")
+    .text("Distance");
+
+  chart_g.selectAll(".bar")
+    .data(yr_lst)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .on("mouseover", onMouseOver) //Add listener for the mouseover event
+    .on("mouseout", onMouseOut)   //Add listener for the mouseout event
+    .attr("x", function(d) { return chart_x(d.year); })
+    .attr("y", function(d) { return chart_y(d.tot_dist); })
+    .attr("width", chart_x.bandwidth())
+    .transition()
+    .ease(d3.easeLinear)
+    .duration(400)
+    .delay(function (d, i) {
+      return i * 50;
+    })
+    .attr("height", function(d) { return chart_height - chart_y(d.tot_dist); });
 }
   
-  //mouseover event handler function
-  function onMouseOver(i, d) {
-      d3.select(this).attr('class', 'highlight');
-      d3.select(this)
-        .transition()     // adds animation
-        .duration(400)
-        // .attr('width', chart_x.bandwidth() + 5)
-        // .attr("y", function(d) { return chart_y(d.tot_dist) - 10; })
-        // .attr("height", function(d) { return chart_height - chart_y(d.tot_dist) + 10; });
-  
-      chart_g.append("text")
-       .attr('class', 'val') 
-       .attr('x', function() {
-           return chart_x(d.year);
-       })
-       .attr('y', function() {
-           return chart_y(d.tot_dist) - 15;
-       })
-       .text(function() {
-           return [ '' +d.tot_dist];  // Value of the text
-       });
-  }
-  
-  //mouseout event handler function
-  function onMouseOut(i, d) {
-      // use the text label class to remove label on mouseout
-      d3.select(this).attr('class', 'bar');
-      // d3.select(this)
-      //   .transition()     // adds animation
-      //   .duration(400)
-      //   .attr('width', chart_x.bandwidth())
-      //   .attr("y", function(d) { return chart_y(d.tot_dist); })
-      //   .attr("height", function(d) { return chart_height - chart_y(d.tot_dist); });
-  
-      d3.selectAll('.val')
-        .remove()
-  }
+//mouseover event handler function
+function onMouseOver(i, d) {
+    d3.select(this).attr('class', 'highlight');
+    //Animation for making hovered bar wider and taller
+    /*d3.select(this)
+      .transition()     // adds animation
+      .duration(400)
+    .attr('width', chart_x.bandwidth() + 5)
+    .attr("y", function(d) { return chart_y(d.tot_dist) - 10; })
+    .attr("height", function(d) { return chart_height - chart_y(d.tot_dist) + 10; });*/
+
+    chart_g.append("text")
+     .attr('class', 'val') 
+     .attr('x', function() {
+         return chart_x(d.year);
+     })
+     .attr('y', function() {
+         return chart_y(d.tot_dist) - 15;
+     })
+     .text(function() {
+         return [ '' +d.tot_dist];  // Value of the text
+     });
+}
+
+//mouseout event handler function
+function onMouseOut(i, d) {
+    // use the text label class to remove label on mouseout
+    d3.select(this).attr('class', 'bar');
+    //Animation for making hovered bar wider and taller
+    /*d3.select(this)
+      .transition()     // adds animation
+      .duration(400)
+      .attr('width', chart_x.bandwidth())
+      .attr("y", function(d) { return chart_y(d.tot_dist); })
+      .attr("height", function(d) { return chart_height - chart_y(d.tot_dist); });*/
+
+    d3.selectAll('.val')
+      .remove()
+}
   
   
